8ac76c53766675d6fca561dc3282f1e586924c38
pedroabs1 <pedroabs1@gmail.com>
2023-03-11 16:25:00 -0400

    limpeza de codigo

--- adivinhação.py
+++ adivinhação.py
@@ -1,46 +1,48 @@
+import random
+
 print(" ********************************")
 print(" Bem vindo ao app de lotéria!")
 print(" ********************************")
-
-tentativas = 1
-#numero_secreto = 42
-#numero_aleatorio = random.random() * 100
-#numero_secreto = round numero_aleatorio
+tentativa = 0
+total_de_tentativas = 0
+rodada = 1
 
 numero_secreto = random.randrange(1, 101)
 
-while(tentativas <= 3):
+nivel = print("Escolha uma dificuldade (1) para facil (2) para medio e (3) para dificil)")
 
-print(" Tentativa número ", tentativas, "de 3")
-print(" Tentativa número {} de 3" .format(tentativas))
-
-chute_str = input("Digite um número! ")
-
-print("Você digitou ", chute_str)
-chute = int(chute_str)
-
-#if(chute == numero_secreto):
-#    print("Parabéns você acertou!")
-#else:
-#    print("Voce errou, tente novamente!")
-#    if(chute < numero_secreto):
-#        print("Tente um número menor!")
-#    else:
-#        print("Tente um número maior!")
-#print("Fim do jogo!")
-
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-
-if(acertou):
-     print("você acertou!")
-     tentativas = 0
+if(nivel == 3):
+    total_de_tentativas = 10    
+elif(nivel == 2):
+    total_de_tentativas = 10    
 else:
-    print("Voce errou, tente novamente!")
-    if(menor):
-        print("Tente um número maior!")
-    elif (maior):
-        print("Tente um número menor!")
-tentativas = tentativas + 1
+    total_de_tentativas == 1
+
+
+for rodada in range(1, tentativa + 1):
+    print(" Tentativa número ", tentativa, "de 3")
+    print(" Tentativa número {} de 3" .format(tentativa))
+
+    chute_str = input("Digite um número! ")
+
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
+
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("você acertou!")
+        total_de_tentativas = 0
+    else:
+        print("Voce errou, tente novamente!")
+        if(menor):
+            print("Tente um número maior!")
+        elif (maior):
+            print("Tente um número menor!")
+    tentativa = tentativa + 1
+
+
 print("Fim do jogo!")
